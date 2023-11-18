e8112b45bf363124fb96ea3c68984c5cb0e7ff52
Vitoriomic <151282692+Vitoriomic@users.noreply.github.com>
2023-11-17 22:40:14 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,20 @@
+
+
+public class Aluno {
+
+   private double notas;
+   private Curso curso;
+
+    public double getNotas() {
+        return notas;
+    }
+
+    public void setNotas(double notas) {
+        this.notas = notas;
+    }
+
+    public void setCurso(Curso curso){
+
+    }
+
+}
